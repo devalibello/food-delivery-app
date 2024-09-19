38cdd398f951eaec7cd6df9d5feb038dad4d7ff0
devalibello <alibelloimoukhuede@gmail.com>
2024-09-19 17:21:51 +0100

    Add routes with express router 1

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 require("dotenv").config();
 
-const express = require("express");
-const foodRouter = require("./routes/foodRoutes")
+const express = require("express")
+const foodRouter = require('./routes/foodRoutes')
 
 //express app
 const app = express()
+
+//parse incoming request into req handler in JSON format when the req has a body.
+//This is used for POST or PATCH requests
+app.use(express.json())
 
 
 //routes middleware
@@ -12,5 +16,5 @@ app.use('/api/foods', foodRouter)
 
 //Listen for requests
 app.listen(process.env.PORT, () => {
-  console.log(`Listening to requests on port ${process.env.PORT}`);
+console.log(`Listening to requests on port ${process.env.PORT}`);
 });
