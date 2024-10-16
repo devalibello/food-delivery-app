0d1c0773957e7265e1024bb0990c659c9891a71b
devalibello <alibelloimoukhuede@gmail.com>
2024-10-17 00:16:11 +0100

    Add banner logo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './assets/components/Navbar/Navbar.jsx'
+import Banner from './assets/components/Banner/Banner.jsx'
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar />
+      <Banner />
     </div>
   )
 }
