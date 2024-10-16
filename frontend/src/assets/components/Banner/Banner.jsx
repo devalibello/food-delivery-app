f6acc38a3bc7829fdf8f1b87367f6d7b665f344b
devalibello <alibelloimoukhuede@gmail.com>
2024-10-17 00:44:38 +0100

    Add banner text

--- frontend/src/assets/components/Banner/Banner.jsx
+++ frontend/src/assets/components/Banner/Banner.jsx
@@ -1,18 +1,16 @@
 import banner from '../../images/banner-image.png'
-import 'Banner.css'
+import './Banner.css'
 
 
 const Banner = () => {
   return (
     <div className="banner">
-      <div className="banner-cover">
             <div className="banner-text">
-                <h1 className="banner-l"></h1>
-                <p className="banner-right"></p>
-                <button>View Menu</button>
+                <h1>Feast Your Senses, <br /> <span className="fast-fresh">Fast and Fress</span></h1>
+                <p>Order your favorite meal today!!!</p>
+                <button className='btn'>View Menu</button>
             </div>
-            <img src={banner} alt="" />
-      </div>
+            <img className='banner-image' src={banner} alt="" />
     </div>
   )
 }
