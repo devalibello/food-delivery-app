bd4642a52e0c67e9336e6a0648d467ad58ebc4a5
devalibello <alibelloimoukhuede@gmail.com>
2024-10-17 01:02:29 +0100

    Add categories

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './assets/components/Navbar/Navbar.jsx'
 import Banner from './assets/components/Banner/Banner.jsx'
+import Categories from './assets/components/Categories/Categories.jsx'
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar />
       <Banner />
+      <Categories />
     </div>
   )
 }
