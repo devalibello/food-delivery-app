662424fd9ee817532d57e243c4404b8253352913
devalibello <alibelloimoukhuede@gmail.com>
2024-10-17 01:02:29 +0100

    Add categories

---
+++ frontend/src/assets/components/Categories/Categories.jsx
@@ -0,0 +1,14 @@
+import './Categories.css'
+
+import React from 'react'
+
+const Categories = () => {
+  return (
+    <div className='categories'>
+      <h2>Explore Our Menu</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br />Velit quae modi, quibusdam eius esse tenetur? Vel accusamus iure nostrum tempore?</p>
+    </div>
+  )
+}
+
+export default Categories
