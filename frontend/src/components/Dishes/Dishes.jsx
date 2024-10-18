0e75880c628de3e884f82c33302505f85d0ff962
devalibello <alibelloimoukhuede@gmail.com>
2024-10-18 16:28:19 +0100

    Add cart context 2 5

--- frontend/src/components/Dishes/Dishes.jsx
+++ frontend/src/components/Dishes/Dishes.jsx
@@ -5,20 +5,19 @@ import FoodItem from "../FoodItem/FoodItem";
 
 const Dishes = () => {
   const { foodList } = useContext(StoreContext);
-  console.log(foodList)
   return (
     <div className="dishes">
       <h2>Your Favorite Dishes</h2>
       <div className="dish-section">
-      {foodList.map((food) => 
+      {foodList.map((food) => {
         <FoodItem 
-            id={food.id}
+            id={food._id}
             name={food.name}
             description={food.description}
             image={food.image}
             price={food.price}
         />
-      )}
+      })}
       </div>
     </div>
   );
--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -9,23 +9,21 @@ const FoodItem = ({id, name, description, price, image}) => {
 const { cartItems, addToCart, removeFromCart } = useContext(StoreContext)
 
   return (
-    <div>
         <div key={id} className="dish-container">
             <img src={image} alt="" />
             {
                 !cartItems[id]
-                ? <img onClick={addToCart(id)} src={assets.addIconWhite} alt="" />
+                ? <img onClick={() => addToCart(id)} src={assets.addIconWhite} alt="" className='add-food-white'/>
                 : <div className="food-counter">
-                    <img onClick={() => addToCart(id)} src={assets.removeIconRed} alt="" className='remove-food' />
-                    <p>{cartItems[id]}</p>
-                    <img onClick={removeFromCart} src={assets.addIconGreen} alt="" className="add-food" />
+                    <img onClick={() => removeFromCart(id)} src={assets.removeIconRed} alt="" className='remove-food' />
+                    <p className="cart-counter-display">{cartItems[id]}</p>
+                    <img onClick={() => addToCart(id)} src={assets.addIconGreen} alt="" className="add-food" />
                 </div>
             }
             <h3>{name}</h3>
             <p>{description}</p>
             <h3>${price}</h3>
       </div>
-    </div>
   )
 }
 
