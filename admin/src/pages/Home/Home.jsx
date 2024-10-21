7cab7f1de1c771de6b39a600e13b150137f7f3cb
devalibello <alibelloimoukhuede@gmail.com>
2024-10-21 22:48:54 +0100

    Add admin sidebar

---
+++ admin/src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Sidebar from '../../components/Sidebar/Sidebar'
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <Sidebar />
+    </div>
+  )
+}
+
+export default Home
