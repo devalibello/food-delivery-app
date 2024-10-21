2d69c663104474b1bfaef9dc79cd0df0a2b9a8ba
devalibello <alibelloimoukhuede@gmail.com>
2024-10-21 23:03:50 +0100

    Add admin list

--- admin/src/Components/Sidebar/Sidebar.jsx
+++ admin/src/Components/Sidebar/Sidebar.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import './Sidebar.css'
+import { assets } from '../../assets/assets'
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
-      
+        <hr />
+        <div className="sidebar-menu">
+            <img src={assets.add_icon} alt="" />
+            <p>Add Food</p>
+        </div>
+        <div className="sidebar-menu">
+            <img src={assets.order_icon} alt="" />
+            <p>Add Food</p>
+        </div>
+        <hr />
     </div>
   )
 }
