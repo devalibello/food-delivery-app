06858eb29951f2ebb489ea2d8360833244df64ae
devalibello <alibelloimoukhuede@gmail.com>
2024-10-21 21:25:48 +0100

    Add checkout

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -3,10 +3,11 @@ import { StoreContext } from '../../context/StoreContext'
 import './Cart.css'
 import { foodList } from '../../assets/assets'
 import { useNavigate } from 'react-router-dom'
+import CartTotal from '../../components/CartTotal/CartTotal'
 
 const Cart = () => {
 
-  const { cartItems, removeFromCart, getTotalAmount } = useContext(StoreContext)
+  const { cartItems, removeFromCart} = useContext(StoreContext)
 
   const navigate = useNavigate()
 
@@ -37,26 +38,7 @@ const Cart = () => {
         }
       })}
       <hr />
-      <div className="cart-buttom">
-        <h2>Cart Total</h2>
-        <div className="cart-total-section">
-          <div className="cart-total-cal">
-            <p>Subtotal:</p>
-            <p className='grand-total'>${getTotalAmount()}</p>
-          </div>        
-          <hr />
-          <div className="cart-total-cal">
-            <p>Delivery Fee:</p>
-            <p className='grand-total'>${getTotalAmount() ? 50 : 0}</p>
-          </div> 
-          <hr />
-          <div className="cart-total-cal">
-            <p>Grand Total:</p>
-            <p className='grand-total'>${getTotalAmount() ? getTotalAmount() + 50 : 0}</p>
-          </div> 
-        </div>
-        <button onClick={() => goToCheckout()} className='checkout-btn'>CHECKOUT</button>
-      </div>
+      <CartTotal payment="CHECKOUT" goToPayment={goToCheckout} />
     </div>
   )
 }
--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import './PlaceOrder.css'
+import CartTotal from '../../components/CartTotal/CartTotal'
+import { useNavigate } from 'react-router-dom'
 
 const PlaceOrder = () => {
+
+  const navigate = useNavigate()
+
+  const goToPayment = () => {
+    navigate('/payment')
+  }
   return (
-    <div>
-      
+    <div className='place-order'>
+    <h2>Delivery Information</h2>
+      <form className="checkout-form">
+        <input type="text" placeholder='First name'/>
+        <input type="text" placeholder='Last name'/>
+        <input type="email" placeholder='Email'/>
+        <input type="text" placeholder='Street'/>
+        <div className="horizontal-fields">
+          <input className='dual-input' type="text" placeholder='City'/>
+          <input className='dual-input' type="text" placeholder='State'/>
+        </div>
+        <div className="horizontal-fields">          
+          <input className='dual-input' type="text" placeholder='zipcode'/>
+          <input className='dual-input' type="text" placeholder='Country'/>
+        </div>
+        <input type="text" placeholder='Phone'/>
+      </form>
+      <div className="cart-totals">
+        <CartTotal payment="PAYMENT" goToPayment={goToPayment} />
+      </div>
     </div>
   )
 }
