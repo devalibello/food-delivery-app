b3a15cb86c68be007e89713fbc6a0324106b7c38
devalibello <alibelloimoukhuede@gmail.com>
2024-10-23 23:34:50 +0100

    List item added 3

--- admin/src/pages/AddItem/AddItem.jsx
+++ admin/src/pages/AddItem/AddItem.jsx
@@ -3,8 +3,28 @@ import './AddItem.css'
 
 const AddItem = () => {
   return (
-    <div>
-      
+    <div className='add-item'>
+        <img src="" alt="" className="add-image" />
+        <form className="add-item-form">
+            <div className="product-info">
+                <p>Product Name</p>
+                <input type="text" placeholder='Type Here' />
+            </div>
+            <div className="product-info">
+                <p>Product Description</p>
+                <input type='longtext' placeholder='Write Here' />
+            </div>
+            <div className="product-category-price">
+                <div className="side-product">
+                    <p>Product Category</p>
+                    <input type='dropdown' placeholder='Write Here' />
+                </div>
+                <div className="side-product">
+                    <p>Product Price</p>
+                    <input type='longtext' placeholder='Type Here' />
+                </div>            
+            </div>
+        </form>
     </div>
   )
 }
