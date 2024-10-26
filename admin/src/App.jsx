e525b1d220f099fd1f733efe8958444bec788b21
devalibello <alibelloimoukhuede@gmail.com>
2024-10-26 21:15:23 +0100

    Update admin panel

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -8,14 +8,16 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
+  const url = 'http://localhost:4000'
+
   return (
     <div className='app'>
       <Navbar />
       <ToastContainer />
       <Sidebar />
         <Routes>
-          <Route path='/add' element={<AddItem />}/>
-          <Route path='/list' element={<ListItem />} />
+          <Route path='/add' element={<AddItem url={url}/>}/>
+          <Route path='/list' element={<ListItem url={url}/>} />
         </Routes>
     </div>
   )
--- admin/src/pages/ListItem/ListItem.jsx
+++ admin/src/pages/ListItem/ListItem.jsx
@@ -2,11 +2,11 @@ import React, { useEffect, useState } from 'react'
 import './ListItem.css'
 import axios from 'axios'
 import { toast } from 'react-toastify'
+import { assets } from '../../assets/assets'
 
-const ListItem = () => {
+const ListItem = ({url}) => {
 
 const [foodList, setFoodList] = useState([])
-const url = 'http://localhost:4000'
 
 const fetchFood = async () => {
   const response = await axios.get(`${url}/api/foods/`)
@@ -16,8 +16,17 @@ const fetchFood = async () => {
   else {
     toast.error("Error Fetching Foods")
   }
-  // console.log(response.data)
 
+}
+
+const deleteFood = async (id) => {
+  const response = await axios.delete(`${url}/api/foods/${id}`)
+  if (response.status == 200) {
+    toast.success("Food Deleted")
+    fetchFood()
+  } else {
+    toast.error("Error Deleting Food")
+  }
 }
 
 useEffect(() => {
@@ -29,15 +38,13 @@ fetchFood()
       {
         foodList.map((food) => 
         <>
-          <ul className='cart-header'>
-            <li><img className='cart-food-image' src={`${url}/images/` + food.image} alt="" /></li>
+          <ul className='list-items-container'>
+            <li><img className='list-items-image' src={`${url}/images/` + food.image} alt="" /></li>
             <li>Name: {food.name}</li>
-            <li>Description: {food.description}</li>
             <li>Price: ${food.price}</li>
             <li>Category: {food.category}</li>
-            <li>Remove</li>
+            <img onClick={() => deleteFood(food._id)} className='list-item-cross' src={assets.remove_icon_red} alt="" />
           </ul>
-          <hr />
         </>
         )
       }
