4942c88aa4d46cd06a47580dc1d7b33de06a11dd
devalibello <alibelloimoukhuede@gmail.com>
2024-12-30 21:44:39 +0100

    Add cart white

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -6,14 +6,19 @@ import { StoreContext } from '../../context/StoreContext'
 
 const FoodItem = ({id, name, description, price, image}) => {
 
-const { cartItems, addToCart, removeFromCart, url } = useContext(StoreContext)
+const { cartItems, addToCart, removeFromCart, url, token } = useContext(StoreContext)
+
+const whiteAddToCart = () => {
+  console.log(token);
+  
+};
 
   return (
         <div key={id} className="dish-container">
             <img className='food-item-image' src={`${url}/images/${image}`} alt="" />
             {
                 !cartItems[id]
-                ? <img onClick={() => addToCart(id)} src={assets.addIconWhite} alt="" className='add-food-white'/>
+                ? <img onClick={() => whiteAddToCart()} src={assets.addIconWhite} alt="" className='add-food-white'/>
                 : <div className="food-counter">
                     <img onClick={() => removeFromCart(id)} src={assets.removeIconRed} alt="" className='remove-food' />
                     <p className="cart-counter-display">{cartItems[id]}</p>
