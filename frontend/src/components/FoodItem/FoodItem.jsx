33ed3a2bf4f64f629b34d4f498d3c9a85611dee8
devalibello <alibelloimoukhuede@gmail.com>
2024-12-30 22:01:12 +0100

    Add cart white logic

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -3,14 +3,18 @@ import './FoodItem.css'
 import { assets } from '../../assets/assets'
 import { useContext } from 'react'
 import { StoreContext } from '../../context/StoreContext'
+import { toast } from 'react-toastify'
 
 const FoodItem = ({id, name, description, price, image}) => {
 
 const { cartItems, addToCart, removeFromCart, url, token } = useContext(StoreContext)
 
-const whiteAddToCart = () => {
-  console.log(token);
-  
+const whiteAddToCart = (id) => {
+  if (token) {
+    addToCart(id)
+  } else {
+    toast.error("You must sign in to add items to cart")
+  }
 };
 
   return (
@@ -18,7 +22,7 @@ const whiteAddToCart = () => {
             <img className='food-item-image' src={`${url}/images/${image}`} alt="" />
             {
                 !cartItems[id]
-                ? <img onClick={() => whiteAddToCart()} src={assets.addIconWhite} alt="" className='add-food-white'/>
+                ? <img onClick={() => whiteAddToCart(id)} src={assets.addIconWhite} alt="" className='add-food-white'/>
                 : <div className="food-counter">
                     <img onClick={() => removeFromCart(id)} src={assets.removeIconRed} alt="" className='remove-food' />
                     <p className="cart-counter-display">{cartItems[id]}</p>
