c04840e86a9fedd48f7e5442eaf43c245d6d8e57
devalibello <alibelloimoukhuede@gmail.com>
2025-01-05 21:36:17 +0100

    update admin panel 2

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg'
 import add_icon from './add_icon.png'
 import order_icon from './order_icon.png'
-import profile_image from './profile_image.png'
+// import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 import remove_icon_red from './remove_icon_red.png'
+import profile_image from './self.png'
 
 export const assets ={
     logo,
